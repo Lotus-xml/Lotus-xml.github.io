72ee969559e8b4b6b1a5122647e70f7c63f434c1
Lotus <92619986+Lotus-xml@users.noreply.github.com>
2023-03-20 19:25:17 -0400

    file i meant to add lol

---
+++ js/hecker.js
@@ -0,0 +1,129 @@
+// visual only
+let hecker=`
+<!DOCTYPE html>
+<html lang="en"
+<head>
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+<meta content="utf-8" http-equiv="encoding">
+<title>Louts Was Here (:</title>
+
+<style>
+.fadeout {
+  opacity: 0;
+  display: block;
+  left:0;
+  right:0;
+  margin-left: auto;
+  margin-right: auto;
+  position: absolute;
+    -webkit-animation: fadeout 2s; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadeout 2s; /* Firefox < 16 */
+        -ms-animation: fadeout 2s; /* Internet Explorer */
+         -o-animation: fadeout 2s; /* Opera < 12.1 */
+            animation: fadeout 2s;
+}
+
+@keyframes fadeout {
+    from { opacity: 1; }
+    to   { opacity: 0; }
+    from {width:60%;}
+    to {width: 10;}
+}
+
+.rainbow {
+  
+  -webkit-animation: rainbow 30s infinite; 
+  
+  -ms-animation: rainbow 30s infinite;
+  
+  animation: rainbow 30s infinite; 
+}
+
+@-webkit-keyframes rainbow{
+
+  0%{color: orange;}
+  10%{color: purple;}
+        20%{color: red;}
+  30%{color: CadetBlue;}
+        40%{color: yellow;}
+  50%{color: coral;}
+        60%{color: green;}
+  70%{color: cyan;}
+  80%{color: DeepPink;}
+  90%{color: DodgerBlue;}
+        100%{color: orange;}
+}
+
+@-ms-keyframes rainbow{
+   0%{color: orange;}
+  10%{color: purple;}
+        20%{color: red;}
+  30%{color: CadetBlue;}
+        40%{color: yellow;}
+  50%{color: coral;}
+        60%{color: green;}
+  70%{color: cyan;}
+  80%{color: DeepPink;}
+  90%{color: DodgerBlue;}
+        100%{color: orange;}
+}
+
+@keyframes rainbow{
+    0%{color: orange;}
+  10%{color: purple;}
+        20%{color: red;}
+  30%{color: CadetBlue;}
+        40%{color: yellow;}
+  50%{color: coral;}
+        60%{color: green;}
+  70%{color: cyan;}
+  80%{color: DeepPink;}
+  90%{color: DodgerBlue;}
+        100%{color: orange;}
+}
+</style>
+</head>
+<body oncontextmenu="return false;" oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false'  style="background-image: url('https://www.drodd.com/images16/black-picture7.jpg'); background-repeat: no-repeat;background-attachment: fixed; background-size: cover;">
+<div style="position: sticky;">
+<h1 class="rainbow" align="center">
+      <script>
+         setTimeout(function(){
+            window.location.href = 'https://lotus-xml.github.io/uwu.html';
+         }, 18000);
+      </script
+<balls>
+<a>Your Website Has Been Seized By Lotus, Take The L!</a><p>
+</p>
+<img src="https://cdn.discordapp.com/icons/948421527471272007/a_31befad9bdcc2c5f7a0e53104fd47f24.gif" alt=Karma>
+<p> <a href= "https://www.instagram.com/karma_.ngo/">Karma On Top! </a>
+</p>
+<p> <a href= "http://catmafia.abuser.eu/">Join The Catmafia Discord Server! </a>
+</p>
+<p> <a href= "https://lotus-xml.github.io/uwu.html"> Click me! </a>
+<br>
+</div>
+</p>
+</h1>
+</balls>
+<marquee behavior="alternate">
+<img src="https://media.discordapp.net/attachments/943914370944106516/954535349810516048/heckercet.jpg?width=595&height=380" alt=HECKER CAT!>
+<p> <a align="center" href="https://media.discordapp.net/attachments/943914370944106516/954535349810516048/heckercet.jpg?width=595&height=380"> :3 Hecker Cat Was Here! </a></p>
+</marquee>
+<audio src="https://lotus-xml.github.io/mp3/m.mp3" controls autoplay="true" loop="true" preload="auto"></audio>
+<script>alert("This website has been hacked by Lotus UwU!")</script>
+          </body>
+
+<iframe
+    src="https://lotus-xml.github.io/index.html"
+    name="targetframe"
+    allowTransparency="true"
+    scrolling="auto"
+    frameborder="10"
+    style="width:100%;height:100%"
+>
+</iframe>
+
+          </html>
+
+`
+document.querySelector('html').innerHTML=hecker;
