b448494e05c6150d591b5293685fa89ffc0d6509
Lotus <92619986+Lotus-xml@users.noreply.github.com>
2023-03-21 15:59:28 -0400

    Update hecker.js

--- js/hecker.js
+++ js/hecker.js
@@ -1,17 +1,28 @@
 // visual only
 let hecker=`
 <!DOCTYPE html>
-<html lang="en"
+<html lang="en">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">
 <meta name="keywords" content="Lotus,cummy,karmango,karma.ngo,catmafia,catmafiaontop,karmaontop,masterc1002,mast3rc1002,mas,html,php,css,javascript,js,deface,defaced,lotusontop,balls,lotuspyc,coding,lotus.pyc,Karma,Hacked,pwned,1337haxor,heckor,seized,hacker,cum,pornhub,mast3rc,uwu,owo,femboys">
 <title>Louts Was Here (:</title>
-<script>alert("This website has been hacked by Lotus UwU!")</script>
 <link href="https://fonts.googleapis.com/css?family=Kelly+Slab" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="https://cdn.prinsh.com/NathanPrinsley-textstyle/nprinsh-stext.css">
 <link rel="stylesheet" href="https://lotus-xml.github.io/css/glitch.css">
-
+</head>
 <style>
+.balls{color:#dbd9d9;font-size:14px;}
+.cummies:hover{color:red}
+marquee.cum{display:;position: fixed; width: 100%; bottom: 0px; font-family: Tahoma; height: 20px; color: white; left: 0px; border-top: 2px solid darkred; padding: 5px; background-color: #000}
+body {
+        color: #FFFFFF;
+        font-family: "Kelly Slab", sans-serif; 
+        font-weight: 60; 
+        height: 80vh;
+        margin: 0; 
+}
 .fadeout {
   opacity: 0;
   display: block;
@@ -85,48 +96,66 @@ let hecker=`
   90%{color: DodgerBlue;}
         100%{color: orange;}
 }
+
+.center {
+     text-align: center; 
+}
+
+}
+
 </style>
-</head>
-<body oncontextmenu="return false;" oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false'  style="background-image: url('https://www.drodd.com/images16/black-picture7.jpg'); background-repeat: no-repeat;background-attachment: fixed; background-size: cover;">
+<body oncontextmenu="return false;" oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false'  style="background-image: url(''); background-repeat: no-repeat;background-attachment: fixed; background-size: cover;">
+<script>
+        setTimeout(function(){
+                window.location.href = 'https://lotus-xml.github.io/uwu.htm';
+                }, 60000);
+</script>
 <div style="position: sticky;">
 <h1 class="rainbow" align="center">
-      <script>
-         setTimeout(function(){
-            window.location.href = 'https://lotus-xml.github.io/uwu.htm';
-         }, 18000);
-      </script>
 <balls>
-<a>Your Website Has Been Seized By Lotus, Take The L!</a><p>
-</p>
-<img src="https://cdn.discordapp.com/icons/948421527471272007/a_31befad9bdcc2c5f7a0e53104fd47f24.gif" alt=Karma>
-<p> <a href= "https://www.instagram.com/karma_.ngo/">Karma On Top! </a>
-</p>
-<p> <a href= "http://catmafia.abuser.eu/">Join The Catmafia Discord Server! </a>
-</p>
-<p> <a href= "https://lotus-xml.github.io/uwu.htm"> Click me! </a>
+<a> UwU </a>
 <br>
-</div>
+<a>Your Website Has Been Seized By Lotus, Take The L!</a>
+<br>
+<img src="https://cdn.discordapp.com/icons/948421527471272007/a_31befad9bdcc2c5f7a0e53104fd47f24.gif" style="white-space: nowrap;" alt="Karma">
+<p style="font-size:20px;white-space: nowrap;"> <a href= "https://www.instagram.com/karma_.ngo/">Check Out Karma's Instagram! </a>
 </p>
+<p style="font-size:20px;white-space: nowrap;"> <a href= "http://catmafia.abuser.eu/">Join The Catmafia Discord Server! </a>
+</p>
+<p style="font-size:20px;white-space: nowrap;"> <a href= "https://lotus-xml.github.io/uwu.htm"> Click me! </a>
+</p>
+<p style="font-size:16px;white-space: nowrap;" class="nprinsley-text-blupelan"><a class="cummies" href="mailto:mast3rc@ccmail.uk">mast3rc@ccmail.uk</a></p>
+<p style="font-size:16px;white-space: nowrap;" class="balls nprinsley-text-redan">Feel Free To Contact Me UwU!</p>
+<div class="center">
+  <div class="glitch" data-text="Karma On Top " style="white-space: nowrap;font-size:18px;"
+  <font color="red" face="5"><font color="blue">Lotus On <font color="white">Top</font>
+  </div>
+<div class="center">
+  <br>
+  <audio src="https://lotus-xml.github.io/mp3/m.mp3" controls autoplay="true" loop="true" preload="auto"></audio>
+</div>
+</div>
 </h1>
 </balls>
+<br>
 <marquee behavior="alternate">
-<img src="https://media.discordapp.net/attachments/943914370944106516/954535349810516048/heckercet.jpg?width=595&height=380" alt=HECKER CAT!>
+<img src="https://media.discordapp.net/attachments/943914370944106516/954535349810516048/heckercet.jpg?width=595&height=380" alt="HECKER CAT!">
 <p> <a align="center" href="https://media.discordapp.net/attachments/943914370944106516/954535349810516048/heckercet.jpg?width=595&height=380"> :3 Hecker Cat Was Here! </a></p>
 </marquee>
-<audio src="https://lotus-xml.github.io/mp3/m.mp3" controls autoplay="true" loop="true" preload="auto"></audio>
-          </body>
-
+</div>
+<br>
 <iframe
     src="https://lotus-xml.github.io/index.htm"
     name="targetframe"
     allowTransparency="true"
     scrolling="auto"
-    frameborder="10"
-    style="width:100%;height:100%"
+    frameborder="1"
+    style="width:100%;height:40%"
 >
 </iframe>
-
-          </html>
+<marquee class="cum"><b style="color: #e34105;font-size:16px;" class="nprinsley-text-rainbowan">root@FemboyFuckers.org:~# echo UwU</b></marquee>
+</div></div>
+</body>
 
 `
 document.querySelector('html').innerHTML=hecker;
